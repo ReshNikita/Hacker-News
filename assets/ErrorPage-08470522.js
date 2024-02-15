2ea13bdd2c61031fda3a917efe4dbaf237927bd3
ReshNikita <ReshNikita@users.noreply.github.com>
2024-02-15 20:56:29 +0000

    deploy: 965645c364fa34d75a2298ba1824bbe800412b35

--- assets/ErrorPage-08470522.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-2eb82654.js";const s=()=>r.jsx("h1",{children:"Error"});export{s as ErrorPage};
---
+++ assets/ErrorPage-aea19d0a.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-2db83e87.js";const s=()=>r.jsx("h1",{children:"Error"});export{s as ErrorPage};
