ea9b4615f1728b70933b8cdf48448e1284ce64be
ReshNikita <rieshietnikov.nikita.1998@mail.ru>
2024-02-16 22:50:14 +0300

    add: header title adaptive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { FC, memo } from "react";
 import { constants } from "../constants";
 
 const Header: FC = () => (
-  <header className="flex items-center gap-500 fixed  z-50  w-full pt-1 pb-6 border-b-4 border-orange-600 bg-header-gray  text-white">
+  <header className="flex items-center gap-500 max-md:gap-200 max-sm:gap-10 fixed  z-50  w-full pt-1 pb-6 border-b-4 border-orange-600 bg-header-gray  text-white">
     <img
       src={constants.IMAGE}
       alt="logo"
