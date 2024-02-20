615da6bef97fc9620e773473891a861b6a50219c
ReshNikita <ReshNikita@users.noreply.github.com>
2024-02-20 20:30:15 +0000

    deploy: a4f6fba466cec984baf562ae4a6c7be58ade6c11

---
+++ assets/ErrorPage-ad230c3f.js
@@ -0,0 +1,1 @@
+import{j as s,c as e}from"./index-23fdc354.js";const t=({error:r})=>s.jsxs("div",{role:"alert",children:[s.jsx("h1",{children:e.ERROR}),s.jsx("p",{children:r.message})]});export{t as ErrorPage};
