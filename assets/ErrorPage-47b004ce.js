feee4d2a03ed7262f2c3ac87e5457a0178f89f3a
ReshNikita <ReshNikita@users.noreply.github.com>
2024-02-21 02:48:44 +0000

    deploy: 42403657120237718f9eef472c937ed4a92c5d4e

---
+++ assets/ErrorPage-47b004ce.js
@@ -0,0 +1,1 @@
+import{j as s,c as e}from"./index-aaaf91c6.js";const t=({error:r})=>s.jsxs("div",{role:"alert",children:[s.jsx("h1",{children:e.ERROR}),s.jsx("p",{children:r.message})]});export{t as ErrorPage};
--- assets/ErrorPage-9d0f47ec.js
+++
@@ -1,1 +0,0 @@
-import{j as s,c as e}from"./index-45b365bd.js";const t=({error:r})=>s.jsxs("div",{role:"alert",children:[s.jsx("h1",{children:e.ERROR}),s.jsx("p",{children:r.message})]});export{t as ErrorPage};
